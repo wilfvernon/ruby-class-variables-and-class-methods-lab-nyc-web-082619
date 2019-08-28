d994c836e491fb1eb341997d589317e1a781dfc6
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-28 00:29:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,17 @@
 require "pry"
 class Song
   attr_accessor :name, :artist, :genre
-
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
   def initialize(song, artist, genre)
   @name = song
   @artist = artist
   @genre = genre
+  @@count += 1
+  @@artists.push(artist)
+  @@genres.push(genres)
   end
 end
