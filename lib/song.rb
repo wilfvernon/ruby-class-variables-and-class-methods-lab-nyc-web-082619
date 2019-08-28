4e8381616d3701cd7032b71c466e11e20bfbb9ce
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-28 00:44:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   
   def self.genre_count
     count = {}
-    @@self.genres do |genre|
+    self.genres do |genre|
       unless count.keys.include?(genre)
         count[genre] = 0
       end
